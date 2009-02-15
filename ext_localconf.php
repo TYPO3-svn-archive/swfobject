1f30b737a4caa336f5cc536cac148fb87317b044
artibella <artibella@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-15 19:50:17 +0000

    Major rewrite of extension * combines rb_flashobject with swfobject * there are now 3 ways to insert records: by Flexform, by Record or by Typoscript * added DAM support * size of Flash movies can be calculated with getID3 (if getid3 or t3getid3 extensions are installed) Todo: * Testing * Multilanguage Integration * Documentation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/swfobject/trunk@16886 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,7 @@ t3lib_extMgm::addUserTSConfig('
 
 t3lib_extMgm::addPItoST43($GLOBALS['_EXTKEY'],'pi1/class.tx_swfobject_pi1.php','_pi1','list_type',1);
 
+
 //###########################################################
 //set TCA for DAM fields which load visual assets into flash
 //###########################################################
